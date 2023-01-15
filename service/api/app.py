4106da870df70d1615a901d75fdb9467540779a6
John Yoon <fedelejohn7008@gmail.com>
2023-01-14 21:40:18 -0700

    Fixed booting issue

--- service/api/app.py
+++ service/api/app.py
@@ -3,12 +3,12 @@ from flask import Flask
 def create_dnn_api():
     app = Flask(__name__)
 
-    @app.route('/', method=['GET', 'POST'])
+    @app.route('/', methods=['GET', 'POST'])
     def root():
         return 'test return 200'
 
     return app
 
 if __name__ == '__main__':
-    app = create_dnn_api()
-    app.run(debug = True, host = 'localhost', port = 8080)
+    dnn_api = create_dnn_api()
+    dnn_api.run(debug=True, host='localhost', port=8080)
