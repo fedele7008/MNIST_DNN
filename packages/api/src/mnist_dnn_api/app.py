902f63c4138f6c5ef53cd68686eb58b1c292d59f
John Yoon <fedelejohn7008@gmail.com>
2023-01-25 21:33:40 -0700

    added Cors extension for flask

--- packages/api/src/mnist_dnn_api/app.py
+++ packages/api/src/mnist_dnn_api/app.py
@@ -1,14 +1,16 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_dnn_api():
     app = Flask(__name__)
+    CORS(app)
 
     @app.route('/', methods=['GET', 'POST'])
     def root():
-        return 'test return 200'
+        return 'test return 200, OK'
 
     return app
 
 if __name__ == '__main__':
     dnn_api = create_dnn_api()
-    dnn_api.run(debug=True, host='localhost', port=8080)
+    dnn_api.run(debug=True, host='0.0.0.0', port=80)
