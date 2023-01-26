94b6857af2f254acdc57250db72a386ab084a7ba
John Yoon <fedelejohn7008@gmail.com>
2023-01-25 21:57:34 -0700

    Update __main__.py

--- packages/dnn/src/mnist_dnn/__main__.py
+++ packages/dnn/src/mnist_dnn/__main__.py
@@ -2,16 +2,19 @@ from mnist_dnn.dataset import MNIST
 from mnist_dnn.layer import *
 from mnist_dnn.model import *
 from mnist_dnn.util.tool import Display
+from timeit import default_timer as timer
 
+start = timer()
 (train_image, train_label), (test_image, test_label) = MNIST.load_data()
+print("Loaded time: ", timer() - start)
 
 print(f"train data size: {train_image.shape[1]}")
 print(f"test data size: {test_image.shape[1]}")
 
 data = []
-for i in range(60):
+for i in range(32):
     title = f'number: {train_label[i]}'
     image = train_image[:, int(i)]
     data.append((title, image))
 
-Display.show_img(data)
+Display.show_img(data, display_resolution=(3072, 1920))
